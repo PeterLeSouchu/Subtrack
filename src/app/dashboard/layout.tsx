a311e40a41a857d70e7c836de63dd960c9083876
PeterLeSouchu <peter22510@gmail.com>
2025-03-11 21:01:13 +0100

    feat: add 3 section

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -39,10 +39,9 @@ const menuItems = [
 
 export default function LayoutDashboard({ children }: { children: ReactNode }) {
   return (
-    <div className='p-3 w-full flex flex-col  h-screen bg-dashboardbg '>
-      <header className='w-full rounded-xl bg-navbar text-white font-semibold '>
-        <NavBar />
-      </header>
+    <div className=' w-full flex flex-col  h-screen bg-dashboardbg '>
+      <NavBar />
+
       <main className='flex-1  h-full '> {children}</main>
     </div>
   );
@@ -84,80 +83,82 @@ function NavBar() {
     }
   }, [pathName]);
   return (
-    <nav className='w-full flex items-center justify-between lg:justify-center px-2 relative'>
-      <Image
-        src='/logowhite.png'
-        className='w-10'
-        width={100}
-        height={100}
-        alt='logo'
-      />
-      <ul className='hidden w-full justify-center gap-20 relative lg:flex '>
-        {menuItems.map((item) => (
-          <li
-            key={item.path}
-            className={`relative flex justify-center items-center gap-1 text-lg ${
-              pathName === item.path ? 'active-link' : ''
-            }`}
-          >
-            {item.icon}
-            <Link href={item.path}>{item.name}</Link>
-          </li>
-        ))}
-        {activeDimensions && (
-          <motion.div
-            className='absolute h-1 bg-white -bottom-2 rounded-2xl'
-            initial={false}
-            animate={{
-              left: activeDimensions.left,
-              width: activeDimensions.width,
-            }}
-            transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-          />
-        )}
-      </ul>
-      <h1 className='font-bold text-2xl lg:hidden'>
-        {displayMobilePageName(pathName)}
-      </h1>
-      <button type='button' onClick={toggleMenu}>
-        {' '}
-        <MenuMobileIcon className='lg:hidden block' width='40' height='40' />
-      </button>
-      {isMenuOpen && (
-        <div
-          className='fixed inset-0 bg-black bg-opacity-50 z-20'
-          onClick={toggleMenu}
-        ></div>
-      )}
-      <motion.div
-        className={`fixed top-0 flex flex-col gap-10 rounded-l  ${
-          isMenuOpen ? 'right-0' : '-right-full'
-        } w-3/5 h-full min-w-48 bg-navbar shadow-lg z-30`}
-        initial={{ right: '-100%' }}
-        animate={{ right: isMenuOpen ? '0' : '-100%' }}
-        transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-      >
-        <div className='flex p-5 justify-end w-full'>
-          {' '}
-          <button onClick={toggleMenu}>
-            <CloseIcon width='22' height='22' />
-          </button>
-        </div>
-        <ul className='flex flex-col gap-14 font-bold items-center '>
+    <header className='w-full p-3  '>
+      <nav className='w-full flex items-center rounded-xl   bg-navbar text-white font-semibold  justify-between lg:justify-center px-2 relative'>
+        <Image
+          src='/logowhite.png'
+          className='w-10'
+          width={100}
+          height={100}
+          alt='logo'
+        />
+        <ul className='hidden w-full justify-center gap-20 relative lg:flex '>
           {menuItems.map((item) => (
-            <li key={item.path} className='text-xl'>
-              <Link
-                href={item.path}
-                onClick={toggleMenu}
-                className='flex items-center gap-2'
-              >
-                {item.icon}
-                {item.name}
-              </Link>
+            <li
+              key={item.path}
+              className={`relative flex justify-center items-center gap-1 text-lg ${
+                pathName === item.path ? 'active-link' : ''
+              }`}
+            >
+              {item.icon}
+              <Link href={item.path}>{item.name}</Link>
             </li>
           ))}
+          {activeDimensions && (
+            <motion.div
+              className='absolute h-1 bg-white -bottom-2 rounded-2xl'
+              initial={false}
+              animate={{
+                left: activeDimensions.left,
+                width: activeDimensions.width,
+              }}
+              transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+            />
+          )}
         </ul>
-      </motion.div>
-    </nav>
+        <h1 className='font-bold text-2xl lg:hidden'>
+          {displayMobilePageName(pathName)}
+        </h1>
+        <button type='button' onClick={toggleMenu}>
+          {' '}
+          <MenuMobileIcon className='lg:hidden block' width='40' height='40' />
+        </button>
+        {isMenuOpen && (
+          <div
+            className='fixed inset-0 bg-black bg-opacity-50 z-20'
+            onClick={toggleMenu}
+          ></div>
+        )}
+        <motion.div
+          className={`fixed top-0 flex flex-col gap-10 rounded-l  ${
+            isMenuOpen ? 'right-0' : '-right-full'
+          } w-3/5 h-full min-w-48 bg-navbar shadow-lg z-30`}
+          initial={{ right: '-100%' }}
+          animate={{ right: isMenuOpen ? '0' : '-100%' }}
+          transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+        >
+          <div className='flex p-5 justify-end w-full'>
+            {' '}
+            <button onClick={toggleMenu}>
+              <CloseIcon width='22' height='22' />
+            </button>
+          </div>
+          <ul className='flex flex-col gap-14 font-bold items-center '>
+            {menuItems.map((item) => (
+              <li key={item.path} className='text-xl'>
+                <Link
+                  href={item.path}
+                  onClick={toggleMenu}
+                  className='flex items-center gap-2'
+                >
+                  {item.icon}
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      </nav>
+    </header>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,26 +7,26 @@ export default function Dashboard() {
   const { data: session } = useSession();
   console.log(session);
   return (
-    <div className='flex  h-full  '>
-      <div className='lg:w-2/3 w-full  '>
-        <section className='flex justify-start items-center   w-full overflow-x-scroll gap-3 p-3'>
-          <article className=' flex flex-col bg-white drop-shadow-lg  rounded-2xl p-3 h-20 text-nowrap '>
+    <div className='flex  h-full   '>
+      <div className='xl:w-2/3 w-full h-full flex flex-col'>
+        <section className='flex justify-start items-center  w-full overflow-x-scroll gap-3 p-3'>
+          <article className='flex flex-1 flex-col bg-white drop-shadow-md rounded-lg p-3 h-20 text-nowrap '>
             <span className='text-[#D6A514] font-black text-4xl'>1300 €</span>
             <h3 className='text-stattext font-bold text-left'>Montant total</h3>
           </article>
-          <article className=' flex flex-col bg-white drop-shadow-lg  rounded-2xl p-3 h-20  text-nowrap'>
+          <article className='flex flex-1 flex-col bg-white drop-shadow-md rounded-lg p-3 h-20 text-nowrap'>
             <span className='text-[#2C4A7B] font-black text-4xl'># 23</span>
             <h3 className='text-stattext font-bold text-left'>
               Nombre de mensualités
             </h3>
           </article>
-          <article className=' flex flex-col bg-white drop-shadow-lg  rounded-2xl p-3 h-20 text-nowrap '>
+          <article className='flex flex-1 flex-col bg-white drop-shadow-md rounded-lg p-3 h-20 text-nowrap'>
             <span className='text-[#43669D] font-black text-4xl flex gap-1 items-center'>
               <BalanceIcon width='30' height='30' /> 23
             </span>
             <h3 className='text-stattext font-bold text-left'>Moyenne</h3>
           </article>
-          <article className=' flex flex-col bg-white drop-shadow-lg  rounded-2xl p-3 h-20 w-auto text-nowrap'>
+          <article className='flex flex-1 flex-col bg-white drop-shadow-md rounded-lg p-3 h-20 w-auto text-nowrap'>
             <span className='text-[#17C058] font-black text-4xl flex gap-1 items-center'>
               <UpIcon width='30' height='30' className='mr-3' /> + 13%
             </span>
@@ -35,9 +35,17 @@ export default function Dashboard() {
             </h3>
           </article>
         </section>
-        <section></section>
+
+        <section className='flex-1 p-3 w-full'>
+          <div className='bg-white drop-shadow-md w-full h-full rounded-md'></div>
+        </section>
       </div>
-      <div className='w-1/3 hidden lg:block'></div>
+
+      <div className='w-1/3 hidden h-full xl:block '>
+        <section className='p-3 h-full rounded-md '>
+          <div className='bg-white h-full w-full drop-shadow-md rounded-md '></div>
+        </section>
+      </div>
     </div>
   );
 }
