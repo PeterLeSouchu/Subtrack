9a1ca808f0f5b3b081b6a13fcdb3535c61eabb76
PeterLeSouchu <peter22510@gmail.com>
2025-03-23 18:56:34 +0100

    refractor: add navbar component in table component

--- src/app/dashboard/components/Tables.tsx
+++ src/app/dashboard/components/Tables.tsx
@@ -45,62 +45,13 @@ export function TableMensuality({
   return (
     <section className={`flex-1 p-3 w-full overflow-hidden block `}>
       <div className='xl:bg-white xl:drop-shadow-md w-full h-full p-4 flex flex-col gap-4 rounded-md md:overflow-hidden overflow-y-scroll'>
-        <div className='flex gap-2 w-full'>
-          <div className='flex flex-1 bg-white border items-center rounded-full w-full p-1'>
-            <input
-              id='search'
-              placeholder='Cherchez une mensualité'
-              type='text'
-              className='rounded-full bg-transparent flex-1 outline-none px-3 w-full min-w-[120px]'
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-            <label htmlFor='search'>
-              <SearchIcon width='20' height='20' />
-            </label>
-          </div>
-          <Select onValueChange={setSelectedCategory}>
-            <SelectTrigger className='w-auto'>
-              <SelectValue placeholder='Catégorie' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value={'all'} className='cursor-pointer border-b'>
-                <p>Toutes</p>
-              </SelectItem>
-              {categoriesData?.map((category, categoryIndex) => (
-                <SelectItem
-                  key={category.id}
-                  value={category.id}
-                  className={`cursor-pointer ${
-                    categoryIndex === categoriesData.length - 1
-                      ? 'border-none'
-                      : 'border-b'
-                  }`}
-                >
-                  <div className='flex flex-row items-center justify-start gap-1'>
-                    <Image
-                      height={20}
-                      width={20}
-                      className='w-5 h-5 object-contain'
-                      src={category.image}
-                      alt='icone categorie'
-                    />
-                    <p>{category.name}</p>
-                  </div>
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <button
-            onClick={() => setOpenCreateModal(true)}
-            type='button'
-            className='flex gap-3 p-2 lg:w-auto lg:h-auto w-10 h-10 justify-center bg-navbar items-center transition lg:hover:bg-blue rounded-full lg:rounded-md font-bold text-white'
-          >
-            <AddIcon width='16' height='16' />
-            <p className='lg:block hidden'> Nouvelle mensualité</p>
-          </button>
-        </div>
-
+        <NavBar
+          setOpenCreateModal={setOpenCreateModal}
+          categoriesData={categoriesData}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Table className='w-full rounded-lg overflow-y-scroll md:table hidden'>
           <TableHeader>
             <TableRow>
@@ -225,5 +176,78 @@ export function TableMensuality({
         )}
       </div>
     </section>
+  );
+}
+
+function NavBar({
+  setOpenCreateModal,
+
+  categoriesData,
+  searchValue,
+  setSearchValue,
+  setSelectedCategory,
+}: {
+  setOpenCreateModal: Dispatch<SetStateAction<boolean>>;
+  categoriesData: CategoryType[] | undefined;
+  searchValue: string;
+  setSearchValue: Dispatch<SetStateAction<string>>;
+  setSelectedCategory: Dispatch<SetStateAction<string>>;
+}) {
+  return (
+    <div className='flex gap-2 w-full'>
+      <div className='flex flex-1 bg-white border items-center rounded-full w-full p-1'>
+        <input
+          id='search'
+          placeholder='Cherchez une mensualité'
+          type='text'
+          className='rounded-full bg-transparent flex-1 outline-none px-3 w-full min-w-[120px]'
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <label htmlFor='search'>
+          <SearchIcon width='20' height='20' />
+        </label>
+      </div>
+      <Select onValueChange={setSelectedCategory}>
+        <SelectTrigger className='w-auto'>
+          <SelectValue placeholder='Catégorie' />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value={'all'} className='cursor-pointer border-b'>
+            <p>Toutes</p>
+          </SelectItem>
+          {categoriesData?.map((category, categoryIndex) => (
+            <SelectItem
+              key={category.id}
+              value={category.id}
+              className={`cursor-pointer ${
+                categoryIndex === categoriesData.length - 1
+                  ? 'border-none'
+                  : 'border-b'
+              }`}
+            >
+              <div className='flex flex-row items-center justify-start gap-1'>
+                <Image
+                  height={20}
+                  width={20}
+                  className='w-5 h-5 object-contain'
+                  src={category.image}
+                  alt='icone categorie'
+                />
+                <p>{category.name}</p>
+              </div>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <button
+        onClick={() => setOpenCreateModal(true)}
+        type='button'
+        className='flex gap-3 p-2 lg:w-auto lg:h-auto w-10 h-10 justify-center bg-navbar items-center transition lg:hover:bg-blue rounded-full lg:rounded-md font-bold text-white'
+      >
+        <AddIcon width='16' height='16' />
+        <p className='lg:block hidden'> Nouvelle mensualité</p>
+      </button>
+    </div>
   );
 }
