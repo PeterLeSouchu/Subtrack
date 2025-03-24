860642d4920a0c2efd0f160113d1c190984e181d
PeterLeSouchu <peter22510@gmail.com>
2025-03-24 13:22:20 +0100

    fix: correction link history

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -19,13 +19,13 @@ export default function History() {
       <div className='p-3 w-4/5  flex flex-col gap-10 pt-10 '>
         {data?.date.map((date, index) => (
           <section key={index}>
-            <span className=' bg-navbar text-white font-bold rounded-xl text-2xl p-2'>
+            <span className=' bg-blue text-white font-bold rounded-xl text-2xl p-2'>
               {date.year}
             </span>
             <div className='mt-6 mb-10  border-t-2 flex items-center gap-3 pt-2'>
               {date.month.map((m, index) => (
                 <Link
-                  href={'history/month'}
+                  href={`history/details?year=${date.year}&month=${m}`}
                   key={index}
                   className='p-2 bg-white transition hover:bg-slate-200 rounded-lg text-xl border font-semibold'
                 >
