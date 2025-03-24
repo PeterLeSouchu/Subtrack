ae2ba1b587a6eb56ffeb23acb96fdfcc96dd13c2
PeterLeSouchu <peter22510@gmail.com>
2025-03-24 09:31:12 +0100

    feat: add create prop on navbar for history use

--- src/app/dashboard/components/Tables.tsx
+++ src/app/dashboard/components/Tables.tsx
@@ -187,12 +187,14 @@ function NavBar({
   searchValue,
   setSearchValue,
   setSelectedCategory,
+  create = true,
 }: {
   setOpenCreateModal: Dispatch<SetStateAction<boolean>>;
   categoriesData: CategoryType[] | undefined;
   searchValue: string;
   setSearchValue: Dispatch<SetStateAction<string>>;
   setSelectedCategory: Dispatch<SetStateAction<string>>;
+  create?: boolean;
 }) {
   return (
     <div className='flex gap-2 w-full'>
@@ -241,14 +243,16 @@ function NavBar({
           ))}
         </SelectContent>
       </Select>
-      <button
-        onClick={() => setOpenCreateModal(true)}
-        type='button'
-        className='flex gap-3 p-2 lg:w-auto lg:h-auto w-10 h-10 justify-center bg-navbar items-center transition lg:hover:bg-blue rounded-full lg:rounded-md font-bold text-white'
-      >
-        <AddIcon width='16' height='16' />
-        <p className='lg:block hidden'> Nouvelle mensualité</p>
-      </button>
+      {create && (
+        <button
+          onClick={() => setOpenCreateModal(true)}
+          type='button'
+          className='flex gap-3 p-2 lg:w-auto lg:h-auto w-10 h-10 justify-center bg-navbar items-center transition lg:hover:bg-blue rounded-full lg:rounded-md font-bold text-white'
+        >
+          <AddIcon width='16' height='16' />
+          <p className='lg:block hidden'> Nouvelle mensualité</p>
+        </button>
+      )}
     </div>
   );
 }
