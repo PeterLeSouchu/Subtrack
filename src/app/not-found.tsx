1c45c190f30686f3f657f64b4e0a0372cecc4f6b
PeterLeSouchu <peter22510@gmail.com>
2025-03-27 23:22:50 +0100

    feat: add 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link';
+
 export default function NotFound() {
-  return <h1>NotFound, vous vous etes perdus</h1>;
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-5xl font-bold text-gray-800'>404</h1>
+      <p className='text-xl text-gray-600 mt-4'>
+        Oups ! Vous vous êtes perdus.
+      </p>
+      <Link
+        href='/'
+        className='mt-6 px-4 py-2 text-white bg-navbar hover:bg-blue rounded-lg hover:bg-blue-700 transition'
+      >
+        Retour à l&apos;accueil
+      </Link>
+    </div>
+  );
 }
