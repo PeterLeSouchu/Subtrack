2c40755dc611c0c227819125f2b3f5638a617484
PeterLeSouchu <peter22510@gmail.com>
2025-03-28 16:00:31 +0100

    style change responsiveo n history page

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -15,14 +15,14 @@ export default function History() {
   return (
     <div className='flex justify-center h-full'>
       {' '}
-      <div className='p-3 w-4/5  flex flex-col gap-10 pt-10 h-full'>
+      <div className='p-3 w-4/5   flex flex-col gap-10 pt-10 h-full'>
         {data?.date && data?.date?.length > 0 ? (
           data?.date.map((date, index) => (
             <section key={index}>
               <span className=' bg-blue text-white font-bold rounded-xl text-2xl p-2'>
                 {date?.year}
               </span>
-              <div className='mt-6 mb-10  border-t-2 flex items-center gap-3 pt-2'>
+              <div className='mt-6 mb-10  flex-wrap border-t-2 flex items-center gap-3 pt-2'>
                 {date.month.map((m, index) => (
                   <Link
                     href={`history/details?year=${date.year}&month=${m}`}
